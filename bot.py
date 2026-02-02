28cfba3e772607d5570f5f2e12cd1f3f1a2be5e2
demajhira-commits <demajhira@gmail.com>
2026-02-02 15:01:57 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -99,7 +99,7 @@ async def modhost(interaction: discord.Interaction, code: str):
         name="Join code",
         value=f"`{code}`",
         inline=False
-    )embed.set_footer(text="Use /endhost to close your lobby")
+    embed.set_footer(text="Use /endhost to close your lobby")
 
     await interaction.response.send_message(embed=embed)
 
@@ -141,3 +141,4 @@ async def mylobby(interaction: discord.Interaction):
     await interaction.response.send_message("\n".join(lines), ephemeral=True)
 
 bot.run(TOKEN)
+
